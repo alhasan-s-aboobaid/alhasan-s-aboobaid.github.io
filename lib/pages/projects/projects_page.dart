9e13f6a589d364a65acb71aa7351233af22693c3
Alhasan Abo Obaid <alhasan.aboobaid@gmail.com>
2025-02-18 18:58:43 +0300

    fix

--- lib/pages/projects/projects_page.dart
+++ lib/pages/projects/projects_page.dart
@@ -17,24 +17,18 @@ class ProjectsSection extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Projects", style: TextStyle(fontSize: sizes.titleFontSize, fontWeight: FontWeight.bold)),
+          Text("Projects under development", style: TextStyle(fontSize: sizes.titleFontSize, fontWeight: FontWeight.bold)),
           SizedBox(height: 20),
           isDesktop
               ? Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ProjectCard(sizes: sizes, title: "Project 1"),
-              ProjectCard(sizes: sizes, title: "Project 2"),
-              ProjectCard(sizes: sizes, title: "Project 3"),
+
             ],
           )
               : Column(
             children: [
-              ProjectCard(sizes: sizes, title: "Project 1"),
-              SizedBox(height: 10),
-              ProjectCard(sizes: sizes, title: "Project 2"),
-              SizedBox(height: 10),
-              ProjectCard(sizes: sizes, title: "Project 3"),
+
             ],
           ),
         ],
