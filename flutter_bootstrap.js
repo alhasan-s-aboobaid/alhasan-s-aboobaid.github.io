5c49c1e55c1e02fdceb2028ed15f510719951eaf
alhasan-s-aboobaid <66334790+alhasan-s-aboobaid@users.noreply.github.com>
2025-04-23 15:41:19 +0000

    Deploying to gh-pages from @ alhasan-s-aboobaid/alhasan-s-aboobaid.github.io@acbeb060919b017e4daa575ea528841a36db2362 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2672382547"
+    serviceWorkerVersion: "613157381"
   }
 });
